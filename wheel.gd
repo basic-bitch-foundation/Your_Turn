215c5ae272b13ee679a261120d532a166f48e656
istoocold <son.of.a.revolver@gmail.com>
2026-05-06 16:39:54 +0530

    check desc.
    rotaing mechanism

---
+++ wheel.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+var rotation_speed: float = 1.9
+var direction: int = 1  # 1 = clockwise, -1 = counter
+
+func _process(delta):
+	rotation += rotation_speed * direction * delta
+	'''
+	so iknow a guy who give visa  b1b2 appointment for very ealy like in 2-3 dayshow did he do i t he take like 200 usd for that 
+	'''
